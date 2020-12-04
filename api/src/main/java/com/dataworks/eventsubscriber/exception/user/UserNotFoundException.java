1280d9d434aade5d80f88541c7df563a37a2ebb4
M de Bruin <michael@mdblogic.com>
2020-12-04 19:19:16 +0100

    Fixes build error

--- api/src/main/java/com/dataworks/eventsubscriber/exception/user/UserNotFoundException.java
+++ api/src/main/java/com/dataworks/eventsubscriber/exception/user/UserNotFoundException.java
@@ -1,6 +1,10 @@
 package com.dataworks.eventsubscriber.exception.user;
 
 public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("User not found!");
+    }
+
     public UserNotFoundException(String email) {
         super(String.format("User with email '%s' not found!", email));
     }
