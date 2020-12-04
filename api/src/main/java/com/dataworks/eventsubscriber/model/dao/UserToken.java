b4d27bd158cb12c5ec89b3cb84a0b38ab892e45a
M de Bruin <michael@mdblogic.com>
2020-12-04 19:07:10 +0100

    Resolves comments

--- api/src/main/java/com/dataworks/eventsubscriber/model/dao/UserToken.java
+++ api/src/main/java/com/dataworks/eventsubscriber/model/dao/UserToken.java
@@ -7,33 +7,15 @@ import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class UserToken extends BaseDao {
 
-    @Getter
-    @Setter
     private int userId;
-
-    @Getter
-    @Setter
     private String token;
-
-    @Getter
-    @Setter
     private TokenType type;
-
-    @Getter
-    @Setter
     private boolean tokenIsUsed;
-
-    @Getter
-    @Setter
     private LocalDateTime created;
-
-    @Getter
-    @Setter
     private LocalDateTime modified;
-
-    @Getter
-    @Setter
     private LocalDateTime deleted;
 }
--- api/src/main/java/com/dataworks/eventsubscriber/service/SendGridProvider.java
+++ api/src/main/java/com/dataworks/eventsubscriber/service/SendGridProvider.java
@@ -18,15 +18,20 @@ public class SendGridProvider implements EmailProvider {
 
     @Value("${spring.sendgrid.api-key}")
     private String sendGridApi;
+    @Value("${spring.sendgrid.api-email}")
+    private String fromEmail;
+    @Value("${spring.sendgrid.email.subject}")
+    private String subject;
+    @Value("${spring.sendgrid.email.content}")
+    private String emailBody;
 
     @Override
     public void send(String email, String token) {
         SendGrid sg = new SendGrid(sendGridApi);
-        Email from = new Email("schoolsendgrid@gmail.com");
+        Email from = new Email(fromEmail);
         Email to = new Email(email);
-        String subject = "Verify your email";
-        System.out.println("token: " + token);
-        var format = String.format("<a href=\"http://localhost:8080/usertokens/verifyuseremail/%s\">Verify your email</a>", token);
+
+        var format = String.format(emailBody, token);
         Content content = new Content("text/html", format);
 
         Mail mail = new Mail(from, subject, to, content);
@@ -36,7 +41,6 @@ public class SendGridProvider implements EmailProvider {
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
             Response response = sg.api(request);
-            System.out.println("SendGrid Api status code: " + response.getStatusCode());
         } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
             System.out.println(Arrays.toString(ioe.getStackTrace()));
