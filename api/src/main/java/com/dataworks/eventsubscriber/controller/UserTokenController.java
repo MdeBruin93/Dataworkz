d2cd9dcc680d817e70c730cde5e4ff57244be77e
M de Bruin <michael@mdblogic.com>
2020-12-07 11:47:08 +0100

    Removes SendGridprovider. Now using gmail

--- api/src/main/java/com/dataworks/eventsubscriber/controller/UserTokenController.java
+++ api/src/main/java/com/dataworks/eventsubscriber/controller/UserTokenController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/usertokens")
+@RequestMapping("api/usertokens")
 @RequiredArgsConstructor
 public class UserTokenController {
     private final UserTokenService userTokenService;
