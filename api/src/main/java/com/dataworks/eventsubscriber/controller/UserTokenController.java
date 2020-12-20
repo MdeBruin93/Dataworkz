9d1babe8564387ecb5f50f29b09fbb3dca873219
rickyvanwaas <0947381@hr.nl>
2020-12-20 19:30:45 +0100

    User token controller

---
+++ api/src/main/java/com/dataworks/eventsubscriber/controller/UserTokenController.java
@@ -0,0 +1,50 @@
+package com.dataworks.eventsubscriber.controller;
+
+import com.dataworks.eventsubscriber.exception.user.UserNotFoundException;
+import com.dataworks.eventsubscriber.exception.user.UserTokenNotFoundException;
+import com.dataworks.eventsubscriber.model.dto.ForgotPasswordDto;
+import com.dataworks.eventsubscriber.service.UserTokenService;
+import com.dataworks.eventsubscriber.service.token.ResetPasswordTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/usertokens")
+@RequiredArgsConstructor
+public class UserTokenController {
+    private final UserTokenService userTokenService;
+    private final ResetPasswordTokenService resetPasswordTokenService;
+
+    @GetMapping("/gettoken/{email}")
+    public ResponseEntity sendEmailVerificationTokenToUser(@PathVariable String email) {
+        if (email.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Need an email in order to verify it.");
+        }
+
+        try {
+            userTokenService.createEmailTokenForUser(email);
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/verifyuseremail/{token}")
+    public ResponseEntity verifyUserEmail(@PathVariable String token) {
+        if (token.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Need a token in order to verify it.");
+        }
+
+        try {
+            return new ResponseEntity(userTokenService.verifyTokenForUser(token), HttpStatus.OK);
+        } catch (UserTokenNotFoundException utnfe) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "UserToken was either not found or incorrect.");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
