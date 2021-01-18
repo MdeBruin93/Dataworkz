228274b0a2c243d3086cc992ab746ef39c1836e1
rickyvanwaas <0947381@hr.nl>
2021-01-18 16:03:22 +0100

    Fixed auth test

--- api/src/test/java/com/dataworks/eventsubscriber/controller/AuthControllerTest.java
+++ api/src/test/java/com/dataworks/eventsubscriber/controller/AuthControllerTest.java
@@ -312,7 +312,6 @@ class AuthControllerTest {
         //given
         var token = "abc";
         var resetPasswordDto = new TokenDto();
-        resetPasswordDto.setToken(token);
         var json = new ObjectMapper().writeValueAsString(resetPasswordDto);
 
         //when
@@ -323,7 +322,7 @@ class AuthControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andDo(print())
-                .andExpect(status().isNotFound());
+                .andExpect(status().isBadRequest());
     }
 
     @Test
