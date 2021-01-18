090d016605da86b99b5f1ea6087722e90e67bef7
rickyvanwaas <0947381@hr.nl>
2021-01-18 17:46:18 +0100

    Fixed possible bug

--- api/src/main/java/com/dataworks/eventsubscriber/service/auth/WebAuthService.java
+++ api/src/main/java/com/dataworks/eventsubscriber/service/auth/WebAuthService.java
@@ -70,7 +70,8 @@ public class WebAuthService implements AuthService {
             return null;
         }
 
-        return userRepository.findByEmail(authentication.getName()).get();
+        return userRepository.findByEmail(authentication.getName())
+                .orElseThrow(UserNotFoundException::new);
     }
 
     @Override
