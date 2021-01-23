b05b43e8cc42be6c066066136266ada6cffae117
rickyvanwaas <0947381@hr.nl>
2021-01-23 18:34:01 +0100

    Finished category service test

--- frontend/eventsubscriber/src/app/categories/services/categories.service.spec.ts
+++ frontend/eventsubscriber/src/app/categories/services/categories.service.spec.ts
@@ -1,19 +1,94 @@
 import { HttpClient } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
-
 import { CategoriesService } from './categories.service';
+import {ICategory} from '@core/interfaces';
+import createSpyObj = jasmine.createSpyObj;
+import SpyObj = jasmine.SpyObj;
+import {defer} from 'rxjs';
+import {Category} from '@core/models';
+import {environment} from '@environments/environment';
 
 describe('CategoriesService', () => {
-  let service: CategoriesService;
-  let httpClient: HttpClient;
+  let categoriesService: CategoriesService;
+  let httpClientMock: SpyObj<HttpClient>;
+
+
+  const categoryObject = {
+    id: 1,
+    name: 'Category',
+    color: 'Red'
+  };
 
   beforeEach(() => {
-    service = new CategoriesService(
-      httpClient
-    );
+    httpClientMock = createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+    categoriesService = new CategoriesService(httpClientMock);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(categoriesService).toBeTruthy();
+  });
+
+  describe('getAll', () => {
+    it('should return a list of categories when success', () => {
+      const expectedCategories: Category[] = [categoryObject];
+
+      httpClientMock.get.and.returnValue(defer(() => Promise.resolve(expectedCategories)));
+
+      categoriesService.getAll().subscribe(
+        categories => expect(categories).toEqual(expectedCategories, 'Expected categories are not equal'),
+        fail
+      );
+      expect(httpClientMock.get).toHaveBeenCalled();
+    });
+  });
+
+  describe('findById', () => {
+    it('should return a object when succes', () => {
+      const expectedCategory: Category = categoryObject;
+
+      httpClientMock.get.and.returnValue(defer(() => Promise.resolve(expectedCategory)));
+
+      categoriesService.findById(1).subscribe(
+        category => expect(category).toEqual(expectedCategory, 'Expected categories are not equal'),
+        fail
+      );
+      expect(httpClientMock.get).toHaveBeenCalled();
+    });
+  });
+
+  describe('save', () => {
+    it('should save a object when category id is not set', () => {
+      const parentCategory: Category = categoryObject;
+      const expectedCategory = {...parentCategory, ...{id: undefined}};
+
+      httpClientMock.post.and.returnValue(defer(() => Promise.resolve(expectedCategory)));
+
+      categoriesService.save(expectedCategory).subscribe(
+        category => expect(category).toEqual(category),
+        fail
+      );
+      expect(httpClientMock.post).toHaveBeenCalled();
+      expect(httpClientMock.put).not.toHaveBeenCalled();
+    });
+
+    it('should update a object when category id is set', () => {
+      const expectedCategory: Category = categoryObject;
+
+      httpClientMock.put.and.returnValue(defer(() => Promise.resolve(expectedCategory)));
+
+      categoriesService.save(expectedCategory).subscribe(
+        category => expect(category).toEqual(category),
+        fail
+      );
+      expect(httpClientMock.post).not.toHaveBeenCalled();
+      expect(httpClientMock.put).toHaveBeenCalled();
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a object', () => {
+      categoriesService.delete(1);
+
+      expect(httpClientMock.delete).toHaveBeenCalled();
+    });
   });
 });
