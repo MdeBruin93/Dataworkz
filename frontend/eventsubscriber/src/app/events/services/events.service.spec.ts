59d9ed36afee4ac35c7ff308f962d4e528c3e492
Kevin Elfering <kevin@thenextapp.nl>
2021-01-23 15:01:41 +0100

    add tests get requests eventsservice

--- frontend/eventsubscriber/src/app/events/services/events.service.spec.ts
+++ frontend/eventsubscriber/src/app/events/services/events.service.spec.ts
@@ -13,13 +13,36 @@ describe('EventsService', () => {
   let httpClientSpy: { get: jasmine.Spy };
   let eventsService: EventsService
 
+  let eventObject = {
+    id: 1,
+    title: 'A',
+    imageUrl: 'test.jpg',
+    description: 'test',
+    date: new Date('January 21, 2021 23:15:30'),
+    maxAmountOfAttendees: 12,
+    euroAmount: 100,
+    category: {
+      id: 1,
+      name: 'test',
+      color: 'ffffff'
+    },
+    categoryId: 1,
+    user: {
+      id: 1,
+      email: 'test@test.nl',
+      role: 'admin',
+      firstName: 'test',
+      lastName: 'tester'
+    }
+  };
+
   beforeEach(() => {
     service = new EventsService(
       http,
       sanitizer
     );
 
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'delete']);
     eventsService = new EventsService(httpClientSpy as any, sanitizer);
   });
 
@@ -28,38 +51,44 @@ describe('EventsService', () => {
   });
 
   it('should return expected events (HttpClient called once)', () => {
-  const expectedEvents: IEventResponse[] =
-    [
-      {
-        id: 1,
-        title: 'A',
-        imageUrl: 'test.jpg',
-        description: 'test',
-        date: new Date('January 21, 2021 23:15:30'),
-        maxAmountOfAttendees: 12,
-        euroAmount: 100,
-        category: {
-          id: 1,
-          name: 'test',
-          color: 'ffffff'
-        },
-        categoryId: 1,
-        user: {
-          id: 1,
-          email: 'test@test.nl',
-          role: 'admin',
-          firstName: 'test',
-          lastName: 'tester'
-        }
-      },
-    ];
+    const expectedEvents: IEventResponse[] = [eventObject];
 
-  httpClientSpy.get.and.returnValue(of(expectedEvents));
+    httpClientSpy.get.and.returnValue(of(expectedEvents));
 
-  eventsService.getAll().subscribe(
-    events => expect(events).toEqual(expectedEvents, 'expected heroes'),
-    fail
-  );
-  expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
-});
+    eventsService.getAll().subscribe(
+      events => expect(events).toEqual(expectedEvents, 'expected events'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return expected event by id (HttpClient called once)', () => {
+    const expectedEvent: IEventResponse = eventObject;
+
+    httpClientSpy.get.and.returnValue(of(expectedEvent));
+
+    eventsService.findById(1).subscribe(
+      event => expect(event).toEqual(expectedEvent, 'expected event'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return expected events by user (HttpClient called once)', () => {
+    const expectedEvents: IEventResponse[] = [eventObject];
+
+    httpClientSpy.get.and.returnValue(of(expectedEvents));
+
+    eventsService.findByUser().subscribe(
+      events => expect(events).toEqual(expectedEvents, 'expected events'),
+      fail
+    );
+    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return sanitized url', () => {
+    const sanitizedUrl = eventsService.sanitize('/test/test.jpg');
+    expect(sanitizedUrl).toEqual('/test/test.jpg');
+  });
+
 });
