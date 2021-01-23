ce0dc85f6e0af907cdf6ca5701d74cd1595af582
Kevin Elfering <kevin@thenextapp.nl>
2021-01-23 21:38:38 +0100

    Show deleted category in filter when category contain events which are planned in the future

--- frontend/eventsubscriber/src/app/core/store/categories/categories.state.ts
+++ frontend/eventsubscriber/src/app/core/store/categories/categories.state.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { State, Selector, StateContext, Action } from '@ngxs/store';
 
 import { CategoriesStateModel } from './categories.state-model';
-import { LoadCategories, LoadCategory, SaveCategory, DeleteCategory } from './categories.actions';
+import { LoadCategories, LoadCategory, SaveCategory, DeleteCategory, LoadFilterCategories } from './categories.actions';
 import { Category } from '@core/models';
+import { IEvent } from '@events/models';
 
 import { CategoriesService } from '@categories/services';
+import { EventsService } from '@events/services';
 
 
 
@@ -13,18 +15,25 @@ import { CategoriesService } from '@categories/services';
   name: 'categories',
   defaults: {
     categories: [],
+    filterCategories: [],
     category: null
   },
 })
 @Injectable()
 export class CategoriesState {
   public constructor(
-    private categoriesService: CategoriesService
+    private categoriesService: CategoriesService,
+    private eventsService: EventsService
   ) { }
 
   @Selector()
   static categories(state: CategoriesStateModel): Category[] {
     return state.categories;
+  }
+
+  @Selector()
+  static filterCategories(state: CategoriesStateModel): Category[] {
+    return state.filterCategories;
   }
 
   @Selector()
@@ -53,6 +62,23 @@ export class CategoriesState {
   public async DeleteCategory(ctx: StateContext<CategoriesStateModel>, { id }: DeleteCategory): Promise<void> {
     await this.categoriesService.delete(id).toPromise();
     await this.getCategories(ctx);
+  }
+
+  @Action(LoadFilterCategories)
+  public async loadFilterCategories(ctx: StateContext<CategoriesStateModel>, {}: LoadFilterCategories): Promise<void> {
+    let categories = await this.categoriesService.getAll().toPromise();
+    const events = await this.eventsService.getAll().toPromise();
+    const usedCategories = events
+      .filter((e: IEvent) => {
+        return new Date(e.date).getTime() > new Date().getTime();
+      })
+      .map((e: IEvent) => {
+        return e.category.id;
+      });
+    categories = categories.filter((c) => {
+      return !c.deleted || usedCategories.includes(c.id);
+    });
+    ctx.patchState({ filterCategories: categories });
   }
 
   private async getCategories(ctx: StateContext<CategoriesStateModel>) {
