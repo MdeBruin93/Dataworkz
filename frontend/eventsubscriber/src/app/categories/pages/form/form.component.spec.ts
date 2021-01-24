500a0e5f51348a5a5a2df8b0fd72c2b401873be1
rickyvanwaas <0947381@hr.nl>
2021-01-24 16:36:15 +0100

    Finished categories

--- frontend/eventsubscriber/src/app/categories/pages/form/form.component.spec.ts
+++ frontend/eventsubscriber/src/app/categories/pages/form/form.component.spec.ts
@@ -3,22 +3,41 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 
 import { FormComponent } from './form.component';
+import {DetailComponent} from "../../../events/pages";
+import createSpyObj = jasmine.createSpyObj;
+import SpyObj = jasmine.SpyObj;
+import Spy = jasmine.Spy;
+import {CategoriesState} from "@core/store";
 
 describe('FormComponent', () => {
+  let storeMock: SpyObj<Store>;
+  let routerMock: SpyObj<Router>;
+  let activatedRouterMock: SpyObj<ActivatedRoute>;
   let component: FormComponent;
-  let store: Store;
-  let router: Router;
-  let route: ActivatedRoute;
 
   beforeEach(() => {
-    component = new FormComponent(
-      store,
-      router,
-      route
-    );
+    storeMock = createSpyObj('Store', ['dispatch']);
+    routerMock = createSpyObj('Router', ['navigate']);
+    activatedRouterMock = createSpyObj('ActivatedRoute', ['toStrings'], {snapshot: {params: {id: undefined}}});
+    component = new FormComponent(storeMock, routerMock, activatedRouterMock);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('ngOnInit', () => {
+    it('should do nothing when id is not set', () => {
+      component.ngOnInit();
+      expect(storeMock.dispatch).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('saveCategory', () => {
+    it('should save a category when function is called', () => {
+      component.saveCategory();
+      expect(storeMock.dispatch).toHaveBeenCalledTimes(2);
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/categories']);
+    });
   });
 });
