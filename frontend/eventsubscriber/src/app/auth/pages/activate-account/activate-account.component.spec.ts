d00fd292bf866c8f9427c1ce10d5d470da0be892
rickyvanwaas <0947381@hr.nl>
2021-01-24 23:04:08 +0100

    Tests

--- frontend/eventsubscriber/src/app/auth/pages/activate-account/activate-account.component.spec.ts
+++ frontend/eventsubscriber/src/app/auth/pages/activate-account/activate-account.component.spec.ts
@@ -3,22 +3,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@auth/services';
 
 import { ActivateAccountComponent } from './activate-account.component';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
+import {defer} from "rxjs";
 
 describe('ActivateAccountComponent', () => {
   let component: ActivateAccountComponent;
-  let router: Router;
-  let route: ActivatedRoute;
-  let authService: AuthService;
+  let routerMock: SpyObj<Router>;
+  let activatedRouteMock: SpyObj<ActivatedRoute>;
+  let authServiceMock: SpyObj<AuthService>;
 
   beforeEach(() => {
+    routerMock = createSpyObj('Router', ['']);
+    activatedRouteMock = createSpyObj('ActivatedRoute', [''], {snapshot: {params: {token: undefined}}});
+    authServiceMock = createSpyObj('AuthService', ['activateAccount']);
     component = new ActivateAccountComponent(
-      router,
-      route,
-      authService
+      routerMock,
+      activatedRouteMock,
+      authServiceMock
     );
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should activate account', () => {
+    authServiceMock.activateAccount.and.returnValue(defer(() => Promise.resolve()));
+
+    component.ngOnInit();
+    expect(authServiceMock.activateAccount).toHaveBeenCalled();
   });
 });
