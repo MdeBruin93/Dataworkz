e8aeacfa41df26423f663b58bfb781e254979814
rickyvanwaas <0947381@hr.nl>
2021-01-24 22:28:41 +0100

    Tests

--- frontend/eventsubscriber/src/app/auth/guards/admin.guard.spec.ts
+++ frontend/eventsubscriber/src/app/auth/guards/admin.guard.spec.ts
@@ -3,16 +3,20 @@ import { Router } from '@angular/router';
 import { AuthService } from '@auth/services';
 
 import { AdminGuard } from './admin.guard';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('AdminGuard', () => {
   let guard: AdminGuard;
-  let authService: AuthService;
-  let router: Router;
+  let authServiceMock: SpyObj<AuthService>;
+  let routerMock: SpyObj<Router>;
 
   beforeEach(() => {
+    authServiceMock = createSpyObj('AuthService', ['isAuthenticated', 'isAdmin']);
+    routerMock = createSpyObj('Router', ['createEmbeddedView', 'clear']);
     guard = new AdminGuard(
-      authService,
-      router
+      authServiceMock,
+      routerMock
     );
   });
 
--- frontend/eventsubscriber/src/app/auth/pages/login/login.component.spec.ts
+++ frontend/eventsubscriber/src/app/auth/pages/login/login.component.spec.ts
@@ -4,18 +4,24 @@ import { Router } from '@angular/router';
 import { Store } from '@ngxs/store';
 
 import { LoginComponent } from './login.component';
+import SpyObj = jasmine.SpyObj;
+import {defer} from 'rxjs';
+import createSpyObj = jasmine.createSpyObj;
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let store: Store;
-  let router: Router;
-  let snackBar: MatSnackBar;
+  let storeMock: SpyObj<Store>;
+  let routerMock: SpyObj<Router>;
+  let snackBarMock: SpyObj<MatSnackBar>;
 
   beforeEach(() => {
+    storeMock = createSpyObj(['dispatch']);
+    routerMock = createSpyObj(['open']);
+    snackBarMock = createSpyObj(['open']);
     component = new LoginComponent(
-      store,
-      router,
-      snackBar
+      storeMock,
+      routerMock,
+      snackBarMock
     );
   });
 
