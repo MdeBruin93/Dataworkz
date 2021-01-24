63f2010ad76be5765002a880bf427b969aa13dcb
Tim Damen <tim.damen80@gmail.com>
2021-01-24 15:34:15 +0100

    Router progress

--- frontend/eventsubscriber/src/app/router.spec.ts
+++ frontend/eventsubscriber/src/app/router.spec.ts
@@ -4,9 +4,10 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { Router } from "@angular/router";
 
 import { routes } from "./app-routing.module";
-import { ForgotPasswordComponent, LoginComponent, RegisterComponent, ResetPasswordComponent } from "./auth";
+import { AuthGuard, AuthService, ForgotPasswordComponent, LoginComponent, RegisterComponent, ResetPasswordComponent } from "./auth";
 import { ActivateAccountComponent } from "@auth/pages/activate-account/activate-account.component";
 import { AppComponent } from "./app.component";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe("Router: App", () => {
   let location: Location;
@@ -23,19 +24,54 @@ describe("Router: App", () => {
     router.initialNavigation();
   });
 
-  it('navigate to "" redirects you to /home', fakeAsync(() => {
-    router.navigate(["login"]).then(() => {
-        console.log(location.path());
+  it('navigate to "" redirects you to /login', fakeAsync(() => {
+    router.navigate([""]).then(() => {
       expect(location.path()).toBe("/login");
-      flush();
     });
+    flush();
   }));
 
-  it('navigate to "search" takes you to /search', fakeAsync(() => {
+  it('navigate to "login" redirects you to /login', fakeAsync(() => {
+    router.navigate(["login"]).then(() => {
+      expect(location.path()).toBe("/login");
+    });
+    flush();
+  }));
+
+  it('navigate to "register" takes you to /register', fakeAsync(() => {
     router.navigate(["/register"]).then(() => {
-    console.log(location.path());
       expect(location.path()).toBe("/register");
     });
     flush();
   }));
+
+  it('navigate to "forgot-password" takes you to /forgot-password', fakeAsync(() => {
+    router.navigate(["/forgot-password"]).then(() => {
+      expect(location.path()).toBe("/forgot-password");
+    });
+    flush();
+  }));
+
+  it('navigate to "reset-password/:token" takes you to reset-password/:token', fakeAsync(() => {
+    router.navigate(["/reset-password/123"]).then(() => {
+      expect(location.path()).toBe("/reset-password/123");
+    });
+    flush();
+  }));
+
+  it('navigate to "activate-password/:token" takes you to /activate-password/:token', fakeAsync(() => {
+    router.navigate(["/activate-password/123"]).then(() => {
+      expect(location.path()).toBe("/activate-password/123");
+    });
+    flush();
+  }));
+
+  it('navigate to "/dashboard" takes you to /dashboard', fakeAsync(() => {
+    router.navigate(["/dashboard"]).then(() => {
+      expect(location.path()).toBe("/dashboard");
+    });
+    flush();
+  }));
+
+
 });
